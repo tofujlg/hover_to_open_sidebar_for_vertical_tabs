f5cabf82e86ae84548eaa2c105a449545914af83
Kohei Okumura <tofuredbull@gmail.com>
2025-03-17 14:32:04 +0900

    chore : Update metadata such as names

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface HoverToRevealSidebarSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: HoverToRevealSidebarSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HoverToRevealSidebar extends Plugin {
+	settings: HoverToRevealSidebarSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HoverToRevealSidebar;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HoverToRevealSidebar) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
