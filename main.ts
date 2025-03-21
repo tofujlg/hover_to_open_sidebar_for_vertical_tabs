8401a9ebc0662898179ecb6f373d401d3eb899c2
Kohei Okumura <tofuredbull@gmail.com>
2025-03-21 10:38:19 +0900

    refactor: remove > main.js commented-out code and unused commands

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-// import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { Plugin } from 'obsidian';
 
 interface HoverToRevealSidebarSettings {
@@ -20,56 +19,6 @@ export default class HoverToRevealSidebar extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This adds a simple command that can be triggered anywhere
-		/*
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-		*/
-
-		// 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		// this.addSettingTab(new HoverToRevealSidebarSettingsTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-
-		
-		
 		this.app.workspace.onLayoutReady(() => {
 
 			// constants
@@ -107,7 +56,6 @@ export default class HoverToRevealSidebar extends Plugin {
 			/**
 			 * Handles left and right sidebar closing on mouse leave
 			 */
-			// TODO:: Type error, fix me later
 			// @ts-ignore
 			this.registerDomEvent(leftSplit.containerEl, "mouseleave", () => {
 				if(this.settings.leftSidebarEnabled){
@@ -137,9 +85,6 @@ export default class HoverToRevealSidebar extends Plugin {
 				}
 			});
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -154,49 +99,3 @@ export default class HoverToRevealSidebar extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-/*
-class HoverToRevealSidebarModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-*/
-
-/*
-class HoverToRevealSidebarSettingsTab extends PluginSettingTab {
-	plugin: HoverToRevealSidebar;
-
-	constructor(app: App, plugin: HoverToRevealSidebar) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Hover to reveal sidebar')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
-*/
